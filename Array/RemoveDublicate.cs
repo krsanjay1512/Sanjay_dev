3999695286310839752bc39977c0a61181842285
Sanjay Kumar <84794705+krsanjay1512@users.noreply.github.com>
2021-10-13 23:47:50 +0530

    Create RemoveDublicate.cs

---
+++ Array/RemoveDublicate.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0,temp=0, j = 0;
+            int[] arr1 = new int[] { 7, 7, 8, 8, 9, 1, 1, 4, 2, 2 };
+            for (i = 0; i < arr1.Length; i++)
+            {
+                for (j = 0; j < arr1.Length; j++)
+                {
+                    if (i == j)
+                        continue;
+                    if (arr1[j] == arr1[i])
+                        break;
+                    temp = arr1[j];
+                }
+                if (arr1.Length == j)
+                {
+                    Console.Write(arr1[i] + " ");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+
+}
