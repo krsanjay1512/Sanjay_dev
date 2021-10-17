fd8a0351dc48849995e330edafc8620762199732
Sanjay Kumar <84794705+krsanjay1512@users.noreply.github.com>
2021-10-17 23:20:11 +0530

    Update BubbleSort.java

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -1,35 +1,30 @@
-class BubbleSort
+using System;
+
+namespace ConsoleApp
 {
-    void bubbleSort(int arr[])
+    public class BubbleSort
     {
-        int n = arr.length;
-        for (int i = 0; i < n-1; i++)
-            for (int j = 0; j < n-i-1; j++)
-                if (arr[j] > arr[j+1])
+        public static void Main(string[] args)
+        {
+            int[] arr = { 78, 55, 45, 98, 13 };
+            int temp;
+            for (int j = 0; j <= arr.Length ; j++)
+            {
+                for (int i = j+1; i <= arr.Length-1 ; i++)
                 {
-                   
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+                    if (arr[j] > arr[i])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
                 }
+            }
+            Console.WriteLine("Sorted:");
+            foreach (int p in arr)
+                Console.Write(p + " ");
+              Console.ReadLine();
+        }
     }
- 
- 
-    void printArray(int arr[])
-    {
-        int n = arr.length;
-        for (int i=0; i<n; ++i)
-            System.out.print(arr[i] + " ");
-        System.out.println();
-    }
- 
-    
-    public static void main(String args[])
-    {
-        BubbleSort ob = new BubbleSort();
-        int arr[] = {64, 34, 25, 12, 22, 11, 90};
-        ob.bubbleSort(arr);
-        System.out.println("Sorted array");
-        ob.printArray(arr);
-    }
+
 }
