b935f58bf3d65ffdec84dc270f86e551a499be1e
Sanjay Kumar <84794705+krsanjay1512@users.noreply.github.com>
2021-10-31 19:00:19 +0530

    Update CountDigitsInNumber.cs

--- LogicalProgramming/CountDigitsInNumber.cs
+++ LogicalProgramming/CountDigitsInNumber.cs
@@ -7,7 +7,8 @@ namespace ConsoleApp
     {
         public static void Main(string[] args)
         {
-            int n = 1234;
+             Console.WriteLine("Enter Number :");
+            int n = int.Parse(Console.ReadLine());
             int count = 0;
             while (n>0)
             {
