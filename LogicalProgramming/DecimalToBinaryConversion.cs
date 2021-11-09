2c2265d552fa066a495350032f1b616fcd2ee018
Sanjay Kumar <84794705+krsanjay1512@users.noreply.github.com>
2021-11-09 22:52:16 +0530

    Update DecimalToBinaryConversion.cs

--- LogicalProgramming/DecimalToBinaryConversion.cs
+++ LogicalProgramming/DecimalToBinaryConversion.cs
@@ -1,1 +1,27 @@
+using System;
+using System.Linq;
 
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter the Decimal Number : ");
+            int number = int.Parse(Console.ReadLine());
+
+            string Result = string.Empty;
+            for (int i = 0; number > 0; i++)
+            {
+                Result = number % 2 + Result;
+                number = number / 2;
+            }
+            Console.WriteLine($"Binary Represenation of the given Number : {Result}");
+
+            Console.ReadKey();
+
+
+        }
+    }
+
+}
