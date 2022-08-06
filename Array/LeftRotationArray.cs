a281d0c1948258be68cca6a9779b368031f01ba6
Sanjay Kumar <84794705+krsanjay1512@users.noreply.github.com>
2022-08-06 23:09:43 +0530

    Create LeftRotationArray.cs

---
+++ Array/LeftRotationArray.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace RotateRightInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = { 1,2,3,4,5};
+            int size = input.Length;
+            int temp;
+            //left rotation
+            for (int j=size-1;j>0;j--)
+            {
+                temp = input[size - 1];
+                input[size - 1] = input[j - 1];
+                input[j - 1] = temp;
+            }
+            foreach (int num in input)
+            {
+                Console.WriteLine("Array after left rotation :"+ num );
+               
+            }
+            Console.ReadLine();
+        }
+    }
+}
