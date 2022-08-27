b47a1f76d30269a2ca867f9d6263fc9db305b10d
Sanjay Kumar <84794705+krsanjay1512@users.noreply.github.com>
2022-08-27 23:37:38 +0530

    Create Factorial.js

---
+++ JavaScript/Factorial.js
@@ -0,0 +1,11 @@
+function printFact()
+{
+    var n=6,fact = 1;
+     
+           for(var i=1;i<=n;i++)
+           {
+            fact = fact*i;            
+           }
+           console.log(fact);
+}
+printFact();
