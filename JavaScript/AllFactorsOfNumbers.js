d976013a3ac56e0882182c9d923d949be24c60bf
Sanjay Kumar <krsanjay1512@gmail.com>
2022-09-14 00:48:03 +0530

    Update AllFactorsOfNumbers.js

--- JavaScript/AllFactorsOfNumbers.js
+++ JavaScript/AllFactorsOfNumbers.js
@@ -1,1 +1,13 @@
-
+function printFactorNo()
+{
+    var sum = 0,r, n=6;
+    for (var i=1;i<=n; i++)
+    {
+        if (n%i == 0)
+        {
+            console.log("Factor Of Number is: "+i);
+        }
+    }
+            
+}
+printFactorNo();
