f3ae3dafc55aa5612f411d1127a5fc92be578996
Sanjay Kumar <krsanjay1512@gmail.com>
2022-09-16 00:02:33 +0530

    Update CountSpace.js

--- JavaScript/String/CountSpace.js
+++ JavaScript/String/CountSpace.js
@@ -1,1 +1,15 @@
-
+function printCountSpace()
+{
+    var count = 0;
+    var str = "hello how are you";
+    for (var i=0;i<str.length; i++)
+    {
+        if (str[i] == ' ')
+        {
+            count++;           
+        }
+    }
+            
+    console.log("No of Space in the given string is : "+count);
+}
+printCountSpace();
