f45d59189c441b31adbea495980063ca914da318
Sanjay Kumar <krsanjay1512@gmail.com>
2022-09-19 00:03:32 +0530

    Update Palindrome.js

--- JavaScript/String/Palindrome.js
+++ JavaScript/String/Palindrome.js
@@ -1,1 +1,19 @@
-
+function Palindrome (str) {
+  var reverse = "";
+   //var str  = "mam";         
+  for ( var i=str.length-1; i >= 0; i--)
+  {
+      reverse = reverse+str[i];
+  }
+  
+  if (str === reverse)
+  {
+      console.log("string is Palindrome.");
+  }
+  else
+  {
+      console.log("string is not Palindrome");
+  }
+   }  
+   
+   Palindrome("mam");
